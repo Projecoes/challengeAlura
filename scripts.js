130fd06a6626f729b8213bfb122bb8b3a9e6c7d0
Projecoes <andrewcoelho2507@gmail.com>
2024-08-02 13:47:11 -0300

    Projeto Completo

---
+++ scripts.js
@@ -0,0 +1,68 @@
+//seleciona o campo de texto
+const textArea = document.querySelector(".textArea");
+const mensagem = document.querySelector(".mensagem");
+const copiar = document.querySelector(".copiar");
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = "";
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    stringEncriptada = stringEncriptada.toLowerCase();
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],
+                matrizCodigo[i][1]);
+
+
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensagem.value = textoDesencriptado;
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1],
+                matrizCodigo[i][0]);
+
+        }
+    }
+    return stringDesencriptada;
+}
+
+function btnCopiar(){
+     // Seleciona a area de texto
+    let mensagem = document.querySelector(".mensagem");
+    // Seleciona o texto na área de texto
+    mensagem.select();
+     // Copia o texto selecionado para a área de transferência
+     document.execCommand("copy");
+     alert("Texto copiado!");
+
+
+}
+
